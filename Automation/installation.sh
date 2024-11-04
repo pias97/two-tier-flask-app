358eb055e3612c4b256ff2830c5931e3b6c44407
Ranabir Devnath <ranabir.devnath@gmail.com>
2024-11-04 23:18:02 +0600

    Update installation.sh

--- Automation/installation.sh
+++ Automation/installation.sh
@@ -4,8 +4,8 @@ sudo apt update -y
 sudo apt upgrade -y
 
 sudo apt install docker.io -y
-sudo chown $USER /var/run/docker.sock
 
 sudo apt install docker-compose -y
+sudo chown $USER /var/run/docker.sock
 echo "Docker installation complete. You may now run Docker commands."
 
