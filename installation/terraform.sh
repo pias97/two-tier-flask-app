5d82f93d14a33ea23cf4e8cda74fa848d62217ce
Ranabir Devnath <ranabir.devnath@gmail.com>
2026-01-01 14:38:44 +0600

    Add installation script for Terraform
    This script updates the system and installs Terraform, including adding the HashiCorp GPG key and repository.

---
+++ installation/terraform.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+
+################################################################
+
+#terraform installation
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+# Verify installation
+terraform --version
+echo "Terraform installation complete."
